64d71ff10e20d030d35f3ff0e464d49a391e28ab
Ridosbey Milian <ridosbeymi@gmail.com>
2019-09-05 13:25:03 -0400

    Adding fix realted to backmarket

--- cenit_wireless/models/models.py
+++ cenit_wireless/models/models.py
@@ -6,82 +6,16 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
+class CenitSaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    bm_id = fields.Char(string=_('Backmarket id'))
+
 
 class CenitSaleOrder(models.Model):
     _inherit = "sale.order"
 
     bm_id = fields.Char(string=_('Backmarket id'))
-
-    # metodo de prueba
-    @api.depends('partner_id')
-    @api.model
-    def customer_verify(self):
-        return {'postdata_arr': [
-            {
-                "ExternalCode": "0002",
-                "Requester": "Odoo",
-                "AccountName": "Extensa-Test-Vincent2",
-                "AccountType": "Levenrancier",
-                "Address": "streetname 4c",
-                "ZipCode": "8570",
-                "City": "Vichte",
-                "Country": "BE",
-                "ContactName": "Niusvel",
-                "PhoneNumber": "+53395088",
-                "Email": "nisuvel@gmail.com",
-                "VATNumber": "0",
-                "BankAccount": "",
-                "Valuta": "EUR"
-            },
-            {
-                "ExternalCode": "0002",
-                "Requester": "Odoo",
-                "AccountName": "Extensa-Test-Vincent2",
-                "AccountType": "Levenrancier",
-                "Address": "streetname 4c",
-                "ZipCode": "8570",
-                "City": "Vichte",
-                "Country": "BE",
-                "ContactName": "Niusvel",
-                "PhoneNumber": "+53395088",
-                "Email": "nisuvel@gmail.com",
-                "VATNumber": "0",
-                "BankAccount": "",
-                "Valuta": "EUR"
-            },
-            {
-                "ExternalCode": "0002",
-                "Requester": "Odoo",
-                "AccountName": "Extensa-Test-Vincent2",
-                "AccountType": "Levenrancier",
-                "Address": "streetname 4c",
-                "ZipCode": "8570",
-                "City": "Vichte",
-                "Country": "BE",
-                "ContactName": "Niusvel",
-                "PhoneNumber": "+53395088",
-                "Email": "nisuvel@gmail.com",
-                "VATNumber": "0",
-                "BankAccount": "",
-                "Valuta": "EUR"
-            },
-            {
-                "ExternalCode": "0002",
-                "Requester": "Odoo",
-                "AccountName": "Extensa-Test-Vincent2",
-                "AccountType": "Levenrancier",
-                "Address": "streetname 4c",
-                "ZipCode": "8570",
-                "City": "Vichte",
-                "Country": "BE",
-                "ContactName": "Niusvel",
-                "PhoneNumber": "+53395088",
-                "Email": "nisuvel@gmail.com",
-                "VATNumber": "0",
-                "BankAccount": "",
-                "Valuta": "EUR"
-            }
-        ]}
 
     @api.model
     def check_order_aviablity(self, order_ids):
