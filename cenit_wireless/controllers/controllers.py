143f0f616372b6910dce111cbbb4c32d5c736bd0
Ramdy <ramdy.rl@gmail.com>
2019-10-07 13:14:53 -0400

    modifying the manage_order, updating stock.picking model and and getting API_KEY and API_SECRET

--- cenit_wireless/controllers/controllers.py
+++ cenit_wireless/controllers/controllers.py
@@ -11,20 +11,38 @@ class CenitWireless(http.Controller):
     def manage_order(self):
 
         data = json.loads(http.request.httprequest.data)
-        API_KEY = '01c7b0b2a5294d4697ed1e587f83fb73'
-        API_SECRET = '747da7fefb1c4c54bdfaf7015a5f07e1'
-        auth = base64.b64encode(b'01c7b0b2a5294d4697ed1e587f83fb73:747da7fefb1c4c54bdfaf7015a5f07e1')
+        API_KEY = http.request.env['ir.config_parameter'].sudo().get_param('odoo_cenit.shipstation.key')
+        #encode_key = base64.b64encode(bytes(API_KEY, 'utf-8'))
+        API_SECRET = http.request.env['ir.config_parameter'].sudo().get_param('odoo_cenit.shipstation.secret')
+        #encode_secret = base64.b64encode(bytes(API_SECRET, 'utf-8'))
         response = requests.get(data.get('resource_url'), auth=(API_KEY, API_SECRET))
-        ss_data = json.loads(response.data)
+        ss_data = json.loads(response.content)
 
-        stock_picking = self.env['stock.picking'].search(
-            [('origin', '=', ss_data.get('shipments')[0].get('orderNumber'))], limit=1).write(
-            {'trackingNumber': ss_data.get('shipments')[0].get('orderNumber'),
-             'servicesCode': ss_data.get('shipments')[0].get('serviceCode')})
-        return {'success': True}
+        oN = ss_data.get('shipments', [{}])[0].get('orderNumber', '')
+        sC = ss_data.get('shipments', [{}])[0].get('serviceCode', '')
+        tN = ss_data.get('shipments', [{}])[0].get('trackingNumber', '')
+
+        stock_picking = http.request.env['stock.picking'].search([('origin', '=', oN)], limit=1)
+
+        if stock_picking :
+            carrier = http.request.env['cenit.wireless.carrier'].search([('shipstation_servicecode', '=', sC)], limit=1)
+
+            if not carrier:
+                carrier = http.request.env['cenit.wireless.carrier'].search([], limit=1)
+
+            stock_picking.write({'carrier_tracking_ref': tN, 'carrier_id': carrier.odoo_carrier.id})
+
+            order = http.request.env['sale.order'].search(['name', '=', oN], limit=1)
+
+            for orderline in order.order_line:
+                orderline.bm_state = 3
+        else:
+            #We should send a message telling the owner the stock.picking doesn't exists
+            return {'success': False, 'message': "Stock Picking doesn't exists"}
+
+        return {'success': True, 'message': 'Stock Picking updated'}
 
 
     @http.route('/cenit_wireless/manage_orderline/', auth='public')
-    def manage_orderline(self, **kw):
-        print(http.request)
+    def manage_orderline(self):
         return "Hello, world"
