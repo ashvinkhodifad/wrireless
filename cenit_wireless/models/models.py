f29638d2db3e9a4923220ebd27b7ffafc8871ace
Ridosbey Milian <ridosbeymi@gmail.com>
2020-05-04 18:11:24 -0400

    changin cotion for checking stock picking type str

--- cenit_wireless/models/models.py
+++ cenit_wireless/models/models.py
@@ -534,7 +534,8 @@ class CenitStockPicking(models.Model):
     def create(self, vals):
         res = super(CenitStockPicking, self).create(vals)
         order = self.env['sale.order'].search([('name', '=', res.origin)], limit=1)
-        if order and res.picking_type_id.name == 'Javelin: Delivery Orders':
+        # if order and res.picking_type_id.name == 'Javelin: Delivery Orders':
+        if order and 'Javelin: Delivery Orders' in str(res.picking_type_id):
             order._send_order_3PL()
         return res
 
